33e5840f2445a626a3853d2e82062d752ef33796
Yakubenka Ivan <palasja@gmail.com>
2024-10-26 16:04:13 +0300

    init: start batleship socket

---
+++ server.js
@@ -0,0 +1,30 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+
+    const d = JSON.parse(data);
+    console.log('received: %s', d);
+    const str = JSON.stringify({
+      name: '123',
+      index: 1,
+      error: false,
+      errorText: '',
+  });
+    const r = {
+      type: "reg",
+      data:str,
+      id: 0,
+    };
+    const res = JSON.stringify(r);
+    console.log('send: %s', typeof res);
+
+    ws.send( res );
+  });
+
+  // ws.send('something');
+});
